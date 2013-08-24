65fccccf55413073b88b2d17599b16425ead8e63
Brock Smedley <brocksmedley@gmail.com>
2013-08-24 16:30:57 -0700

    Feature to copy from clipboard

--- RSS Reader/RSS Reader/Program.cs
+++ RSS Reader/RSS Reader/Program.cs
@@ -9,9 +9,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace RSS_Reader
 {
+    class Clipboard
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetClipboardData(uint uFormat);
+        [DllImport("user32.dll")]
+        static extern bool IsClipboardFormatAvailable(uint format);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool OpenClipboard(IntPtr hWndNewOwner);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool CloseClipboard();
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GlobalLock(IntPtr hMem);
+        [DllImport("kernel32.dll")]
+        static extern bool GlobalUnlock(IntPtr hMem);
+
+        const uint CF_UNICODETEXT = 13;
+
+        public static string GetText()
+        {
+            if (!IsClipboardFormatAvailable(CF_UNICODETEXT))
+                return null;
+            if (!OpenClipboard(IntPtr.Zero))
+                return null;
+
+            string data = null;
+            var hGlobal = GetClipboardData(CF_UNICODETEXT);
+            if (hGlobal != IntPtr.Zero)
+            {
+                var lpwcstr = GlobalLock(hGlobal);
+                if (lpwcstr != IntPtr.Zero)
+                {
+                    data = Marshal.PtrToStringUni(lpwcstr);
+                    GlobalUnlock(lpwcstr);
+                }
+            }
+            CloseClipboard();
+
+            return data;
+        }
+    }
+
     class story
     {
         public string title, link, description;
@@ -34,83 +77,107 @@ namespace RSS_Reader
 
         static void readRSS()
         {
-            Console.WriteLine("Enter an RSS URL to be read.");
+            ///sorry.
+            Console.WriteLine("Enter an RSS URL to be read. Enter 'P' to paste from clipboard.");
 
             url = Console.ReadLine();
-            if (!url.EndsWith(".rss"))
+            Debug.WriteLine("url: " + url);
+
+            int n = 1;
+            switch (n)
             {
-                Console.WriteLine("\nNot a valid URL\n\n");
-                readRSS();
-            }
-            else
-            {
-                //create new XMLTextReader from specified URL (RSS FEED)
-                rssReader = new XmlTextReader(url);
-                rssDoc = new XmlDocument();
+                case 3://shit works
+                    //create new XMLTextReader from specified URL (RSS FEED)
+                    rssReader = new XmlTextReader(url);
+                    rssDoc = new XmlDocument();
 
-                //load the XML content in to an XMLDocument
-                try
-                {
-                    rssDoc.Load(rssReader);
-                }
-                catch
-                {
-                    Console.WriteLine("\nNot a valid RSS page\n");
-                    readRSS();
-                }
-
-                //loop for the <rss> tag
-                for (int i = 0; i < rssDoc.ChildNodes.Count; i++)
-                {
-                    //if it's the rss tag
-                    if (rssDoc.ChildNodes[i].Name == "rss")
+                    //load the XML content in to an XMLDocument
+                    try
                     {
-                        //rss tag found
-                        nodeRss = rssDoc.ChildNodes[i];
+                        rssDoc.Load(rssReader);
                     }
-                }
-
-                //loop for the channel tag
-                for (int i = 0; i < nodeRss.ChildNodes.Count; i++)
-                {
-                    if (nodeRss.ChildNodes[i].Name == "channel")
+                    catch
                     {
-                        //channel tag found
-                        nodeChannel = nodeRss.ChildNodes[i];
+                        Console.WriteLine("\nNot a valid RSS page\n");
+                        readRSS();
                     }
-                }
 
-                //set the labels with information from inside the nodes
-                string title = "Title: " + nodeChannel["title"].InnerText;
-                string language = "Language: " + nodeChannel["language"].InnerText;
-                string link = "Link: " + nodeChannel["link"].InnerText;
-                string description = "Description: " + nodeChannel["description"].InnerText;
-
-                //loop for the title, link, description, and all the other tags
-                for (int i = 0; i < nodeChannel.ChildNodes.Count; i++)
-                {
-                    if (nodeChannel.ChildNodes[i].Name == "item")
+                    //loop for the <rss> tag
+                    for (int i = 0; i < rssDoc.ChildNodes.Count; i++)
                     {
-                        nodeItem = nodeChannel.ChildNodes[i];
-                        //come back to this
-                        story thisStory = new story();
-                        thisStory.title = nodeItem["title"].InnerText;
-                        thisStory.link = nodeItem["link"].InnerText;
-                        thisStory.description = nodeItem["description"].InnerText.Split(new string[] { "&lt", "<div", "FULL STORY" }, StringSplitOptions.None)[0];
-
-                        stories.Add(thisStory);
+                        //if it's the rss tag
+                        if (rssDoc.ChildNodes[i].Name == "rss")
+                        {
+                            //rss tag found
+                            nodeRss = rssDoc.ChildNodes[i];
+                        }
                     }
-                }
 
-                //URL Title
-                Console.WriteLine(title + "\n" + language + "\n" + link + "\n" + description + "\n\n\n");
+                    //loop for the channel tag
+                    for (int i = 0; i < nodeRss.ChildNodes.Count; i++)
+                    {
+                        if (nodeRss.ChildNodes[i].Name == "channel")
+                        {
+                            //channel tag found
+                            nodeChannel = nodeRss.ChildNodes[i];
+                        }
+                    }
 
-                foreach (story s in stories)
-                {
-                    Console.WriteLine("Title: " + s.title + "\nLink: " + s.link + "\n" + "Description: " + s.description + "\n");
-                }
+                    //set the labels with information from inside the nodes
+                    string title = "Title: " + nodeChannel["title"].InnerText;
+                    string language = "Language: " + nodeChannel["language"].InnerText;
+                    string link = "Link: " + nodeChannel["link"].InnerText;
+                    string description = "Description: " + nodeChannel["description"].InnerText;
 
-                Console.Read();
+                    //loop for the title, link, description, and all the other tags
+                    for (int i = 0; i < nodeChannel.ChildNodes.Count; i++)
+                    {
+                        if (nodeChannel.ChildNodes[i].Name == "item")
+                        {
+                            nodeItem = nodeChannel.ChildNodes[i];
+                            story thisStory = new story();
+                            thisStory.title = nodeItem["title"].InnerText;
+                            thisStory.link = nodeItem["link"].InnerText;
+                            thisStory.description = nodeItem["description"].InnerText.Split(new string[] { "&lt", "<div", "FULL STORY" }, StringSplitOptions.None)[0];
+
+                            stories.Add(thisStory);
+                        }
+                    }
+
+                    //URL Title
+                    Console.WriteLine(title + "\n" + language + "\n" + link + "\n" + description + "\n\n\n");
+
+                    foreach (story s in stories)
+                    {
+                        Console.WriteLine("Title: " + s.title + "\nLink: " + s.link + "\n" + "Description: " + s.description + "\n");
+                    }
+
+                    Console.Read();
+                    break;
+
+                case 2://url is copied from clipboard
+                    //get text from clipboard
+                    url = Clipboard.GetText();
+                    Console.WriteLine("Is this URL correct? (y/n): " + url);
+                    string correct = Console.ReadLine();
+                    if (correct.ToLower() != "y" || !correct.ToLower().Contains('y'))
+                        readRSS();
+                    goto case 3;
+
+                case 1://url is typed
+                    if (url.EndsWith(".rss") && url.ToString().StartsWith("http"))
+                    {
+                        goto case 3;
+                    }
+                    else if (url.ToUpper() == "P")
+                        goto case 2;
+                    
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid URL");
+                    break;
+
             }
         }
     }
